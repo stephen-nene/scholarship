8d944e89e1fbabdb094cbc9caf32d79a79f2697b
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-12-09 19:28:30 +0300

    Allow Render domain in config.hosts

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -93,11 +93,17 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [:id]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "onrender.com",   
+    /.*\.onrender\.com/ 
+  ]
+#   config.hosts += [
+#   "scholarship-api-3eri.onrender.com",
+#   "localhost",
+#   "127.0.0.1"
+# ]
+
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
