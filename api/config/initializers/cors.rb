da1e9cdb1cb48077cada122595325a7be86a6c70
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-12-10 12:29:43 +0300

    new cors

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,25 +1,10 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  shared_options = {
-    resource: "*",
-    headers: :any,
-    methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    credentials: true
-  }
-
-  allowed_origins = [
-    "http://localhost:5173",
-    "https://scholarship-06st.onrender.com",
-  ]
-
   allow do
-    origins(*allowed_origins)
-    shared_options.each { |key, value| send(key, value) }
+    origins "http://localhost:5173", "https://scholarship-06st.onrender.com"
+    
+    resource "*", 
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
